0cd6065d0f9c258f76db8d6aee9ef0e7e514dec1
SergeyBildanov <troktrokov@gmail.com>
2024-06-19 16:39:14 +0400

    get|set changes

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,16 +1,15 @@
 export default class Character {
-    constructor(){
-        this.setAttack = function(){
-            this.attack = 100;
-        };
-        this.getAttack = function(x) {
-            let newAttack = this.attack;
-            newAttack = newAttack*(1- 0.1*(x-1)); 
-            if(this.stoned){
-              return newAttack - 5*Math.log2(x);
-            }
-            return newAttack;
-        };
+    constructor(distance){
+        this.distance = distance;
+    }
+    set attackPoints(x){
+        this.attack = x;
+    }
+    get attackPoints(){
+        if(this.stoned){
+            return this.attack*(1 - 0.1*(this.distance - 1)) - Math.log2(this.distance) * 5;
+        }
+        return this.attack*(1 - 0.1*(this.distance-1))
     }
     set stoned(status){
         this.isStoned = status;
--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -1,28 +1,28 @@
 import Character from "../Character";
 
 test("Setting stone", ()=>{
-    let character = new Character();
+    let character = new Character(4);
     character.stoned = true;
     expect(character.isStoned).toBe(true);
 })
 test("Getting stone", ()=>{
-    let character = new Character();
+    let character = new Character(4);
     character.stoned = true;
     expect(character.stoned).toBe(true);
 })
 test("setting attack", ()=>{
-  let character = new Character();
-  character.setAttack();
+  let character = new Character(4);
+  character.attackPoints = 100;
   expect(character.attack).toBe(100);
 })
 test("getting attack", ()=>{
-  let character = new Character();
-  character.setAttack();
-  expect(character.getAttack(4)).toBe(70);
+  let character = new Character(4);
+  character.attackPoints = 100;
+  expect(character.attackPoints).toBe(70);
 })
 test("getting stoned attack", ()=>{
-  let character = new Character();
-  character.setAttack();
+  let character = new Character(4);
+  character.attackPoints = 100;
   character.stoned = true;
-  expect(character.getAttack(4)).toBe((100*(1-0.1*3))-Math.log2(4)*5);
+  expect(character.attackPoints).toBe((100*(1-0.1*3))-Math.log2(4)*5);
 })
